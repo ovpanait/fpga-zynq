00f63d89f45f753122779228b477094e155c9044
Ovidiu Panait <ovidiu.panait@windriver.com>
2018-09-11 09:26:25 +0300

    scripts: Add hdf -> dts generator script
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

---
+++ Scripts/cdts.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Generate dts for Xilinx Zynq
+
+set -e
+
+# Hardcoded
+VIV_SDK="/home/ovidiu/Xilinx/Tools/SDK/2018.2/settings64.sh"
+XIL_SDK="/home/ovidiu/Xilinx/ArtyZ7/device-tree-xlnx"
+
+if [ "$#" -ne 1 ];then
+	echo "Usage: cdts.sh <path-to-hdf>"
+	exit 1
+fi
+
+HDF_FILE="$1"
+
+# Source Vivado SDK
+. "${VIV_SDK}"
+
+# Generate dts
+hsi <<EOF
+open_hw_design "${HDF_FILE}"
+set_repo_path "${XIL_SDK}"
+create_sw_design device-tree -os device_tree -proc ps7_cortexa9_0
+generate_target -dir my_dts
+EOF
