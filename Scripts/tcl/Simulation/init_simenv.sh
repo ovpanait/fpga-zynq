adc723bd3015f2b009bd2113b0f185bcfe67dee7
Ovidiu Panait <ovpanait@gmail.com>
2018-10-22 22:42:22 +0300

    Scripts: init_simenv.sh: get VIVADO_SDK from env
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- Scripts/tcl/Simulation/init_simenv.sh
+++ Scripts/tcl/Simulation/init_simenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VIVADO_SDK="$HOME/Xilinx/Tools/Vivado/2018.2/settings64.sh"
+export VIVADO_SDK="${VIVADO_SDK:-$HOME/Xilinx/Tools/Vivado/2018.2/settings64.sh}"
 
 source "${VIVADO_SDK}"
 export SIM_PATH="$(pwd)"
