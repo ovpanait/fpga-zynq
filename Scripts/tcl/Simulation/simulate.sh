434e4f018f5d0a197c630e9ee8b6bba1fa886686
Ovidiu Panait <ovpanait@gmail.com>
2018-11-10 14:46:50 +0200

    Scripts: simulate.sh: Clean simulation leftovers
    Clean previous simulation files before starting a new one.
    
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- Scripts/tcl/Simulation/simulate.sh
+++ Scripts/tcl/Simulation/simulate.sh
@@ -6,4 +6,5 @@ if [ "$1" == "export_sim" ];then
     vivado -mode tcl -source "$SCRIPTS_TOP"/Simulation/export_sim.tcl -nolog -nojournal
 fi
 
-cd ./outputs/export_sim/xsim && ./tb_main.sh
+cd ./outputs/export_sim/xsim
+./tb_main.sh -reset_run && ./tb_main.sh
