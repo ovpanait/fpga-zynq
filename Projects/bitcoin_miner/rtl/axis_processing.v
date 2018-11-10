b501e185589360934ad394751ea6879d7080e76a
Ovidiu Panait <ovpanait@gmail.com>
2018-11-10 15:16:40 +0200

    bitcoin_miner: axis_processing.v: Remove old / useless comments
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- Projects/bitcoin_miner/rtl/axis_processing.v
+++ Projects/bitcoin_miner/rtl/axis_processing.v
@@ -5,7 +5,7 @@ module test_ip #
    /*
     * Master side parameters
     */
-   // Width of S_AXIS address bus. The slave accepts the read and write addresses of width C_M_AXIS_TDATA_WIDTH.
+   // Width of master side bus
    parameter integer C_M_AXIS_TDATA_WIDTH = 32,
    // Start count is the number of clock cycles the master will wait before initiating/issuing any transaction.
    parameter integer C_M_START_COUNT = 32,
@@ -13,50 +13,35 @@ module test_ip #
    /*
     * Slave side parameters
     */
-   // AXI4Stream sink: Data Width
+   // Width of slave side bus
    parameter integer C_S_AXIS_TDATA_WIDTH = 32
    )
    (
-    // Users to add ports here
     output reg [3:0] 				 led,
-    // User ports ends
+
 
     /*
      * Master side ports
      */
 
-    // Global ports
     input wire 					 m00_axis_aclk,
-    //
     input wire 					 m00_axis_aresetn,
-    // Master Stream Ports. TVALID indicates that the master is driving a valid transfer, A transfer takes place when both TVALID and TREADY are asserted.
     output wire 				 m00_axis_tvalid,
-    // TDATA is the primary payload that is used to provide the data that is passing across the interface from the master.
     output wire [C_M_AXIS_TDATA_WIDTH-1 : 0] 	 m00_axis_tdata,
-    // TSTRB is the byte qualifier that indicates whether the content of the associated byte of TDATA is processed as a data byte or a position byte.
     output wire [(C_M_AXIS_TDATA_WIDTH/8)-1 : 0] m00_axis_tstrb,
-    // TLAST indicates the boundary of a packet.
     output wire 				 m00_axis_tlast,
-    // TREADY indicates that the slave can accept a transfer in the current cycle.
     input wire 					 m00_axis_tready,
 
     /*
      * Slave side ports
      */
 
-    // AXI4Stream sink: Clock
     input wire 					 s00_axis_aclk,
-    // AXI4Stream sink: Reset
     input wire 					 s00_axis_aresetn,
-    // Ready to accept data in
     output wire 				 s00_axis_tready,
-    // Data in
     input wire [C_S_AXIS_TDATA_WIDTH-1 : 0] 	 s00_axis_tdata,
-    // Byte qualifier
     input wire [(C_S_AXIS_TDATA_WIDTH/8)-1 : 0]  s00_axis_tstrb,
-    // Indicates boundary of last packet
     input wire 					 s00_axis_tlast,
-    // Data is in valid
     input wire 					 s00_axis_tvalid
     );
 
@@ -69,21 +54,20 @@ module test_ip #
       end
    endfunction
 
-   // Total number of input data.
+   // Input FIFO size (slave side)
    localparam NUMBER_OF_INPUT_WORDS  = 20;
+   // Output FIFO size (master side)
    localparam NUMBER_OF_OUTPUT_WORDS = 8;
 
    // bit_num gives the minimum number of bits needed to address 'NUMBER_OF_INPUT_WORDS' size of FIFO.
    localparam bit_num  = clogb2(NUMBER_OF_INPUT_WORDS-1);
-   // Define the states of state machine
-   // The control state machine oversees the writing of input streaming data to the FIFO,
-   // and outputs the streaming data from the FIFO
-   parameter [1:0] IDLE = 1'b0,        // This is the initial/idle state
 
-     WRITE_FIFO  = 1'b1, // In this state FIFO is written with the
-     // input stream data S_AXIS_TDATA
+   // Control state machine states
+   parameter [1:0] IDLE = 1'b0, // Initial/idle state
 
-     PROCESS_STUFF = 2'b11, // In this state data is being processed
+     WRITE_FIFO  = 1'b1, // Input FIFO is written with the input stream data S_AXIS_TDATA
+
+     PROCESS_STUFF = 2'b11, // Data is being processed and placed into the output FIFO
 
      MASTER_SEND = 2'b10; // Master is sending processed data
 
@@ -92,7 +76,6 @@ module test_ip #
    /*
     * Master side signals
     */
-   // Example design FIFO read pointer
    reg [bit_num-1:0] read_pointer;
 
    // AXI Stream internal signals
@@ -105,22 +88,17 @@ module test_ip #
    reg [C_M_AXIS_TDATA_WIDTH-1 : 0] out_stream_data_fifo [0 : NUMBER_OF_OUTPUT_WORDS - 1];
    reg [C_M_AXIS_TDATA_WIDTH-1 : 0] stream_data_out;
    wire 			    tx_en;
-   //The master has issued all the streaming data stored in FIFO
+
    reg 				    tx_done;
 
    /*
     * Slave side signals
     */
    wire 			    axis_tready;
-   // State variable
    reg [1:0] 			    mst_exec_state;
-   // FIFO implementation signals
    genvar 			    byte_index;
-   // FIFO write enable
    wire 			    fifo_wren;
-   // FIFO write pointer
    reg [bit_num-1:0] 		    write_pointer;
-   // sink has accepted all the streaming data and stored in FIFO
    reg 				    writes_done;
 
    reg 				    processing_done;
@@ -137,9 +115,6 @@ module test_ip #
 	else
 	  case (mst_exec_state)
 	    IDLE:
-	      // The sink starts accepting tdata when
-	      // there tvalid is asserted to mark the
-	      // presence of valid streaming data
 	      if (s00_axis_tvalid)
 	        begin
 	           mst_exec_state <= WRITE_FIFO;
@@ -264,8 +239,6 @@ module test_ip #
 	       begin
 	          if ((write_pointer == NUMBER_OF_INPUT_WORDS-1) || s00_axis_tlast)
 	            begin
-	               // reads_done is asserted when NUMBER_OF_INPUT_WORDS numbers of streaming data
-	               // has been written to the FIFO which is also marked by S_AXIS_TLAST(kept for optional usage).
 	               writes_done <= 1'b1;
 	            end
 	          else begin
@@ -335,7 +308,7 @@ module test_ip #
    assign blk_nonce = in_stream_data_fifo[19];
    
    // Map out_stream_data_fifo to bitcoin_block outputs
-   genvar 		 j; //This is our generate loop variable.
+   genvar 		 j;
    generate for (j = 0; j < 8; j=j+1) begin
       always @(posedge s00_axis_aclk) begin
 	 if (bitcoin_done == 1'b1) begin
