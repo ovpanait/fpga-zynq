de88b3e263ccdac0ffb6fd79e2665ea646b9e0ee
Ovidiu Panait <ovpanait@gmail.com>
2018-11-10 14:52:09 +0200

    axis-processing: tb: Use new tester API
    Test functionality with real, non-randomized transactions.
    
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- Projects/bitcoin_miner/testbench/axis-processing/tb_main.sv
+++ Projects/bitcoin_miner/testbench/axis-processing/tb_main.sv
@@ -9,6 +9,8 @@ import axi4stream_vip_pkg::*;
 import design_1_axi4stream_vip_0_0_pkg::*;
 import design_1_axi4stream_vip_1_0_pkg::*;
 
+`include "test_fc.vh"
+
 module tb_main(
 	       );
 
@@ -47,6 +49,17 @@ module tb_main(
    // Reset signal
    bit                                     reset;
 
+   // Test signals
+   reg [7:0] 				   data_out[];
+   
+   reg [31:0] 				   blk_version;
+   reg [255:0] 				   prev_blk_header_hash;
+   reg [255:0] 				   merkle_root_hash;
+   reg [31:0] 				   blk_time;
+   reg [31:0] 				   blk_nbits;
+   reg [31:0] 				   blk_nonce;
+
+   wire [255:0] 			   bitcoin_blk;
    // instantiate bd
    design_1_wrapper DUT(
 			.aresetn(reset),
@@ -82,24 +95,53 @@ module tb_main(
       
       mst_agent.start_master();
       slv_agent.start_slave();
-
+      
+      // Test 1
+      blk_version = 32'h02000000;
+      prev_blk_header_hash = 256'h671D0E2FF45DD1E927A51219D1CA1065C93B0C4E8840290A0000000000000000;
+      merkle_root_hash = 256'h2CD900FC3513260DF5BD2EABFD456CD2B3D2BACE30CC078215A907C045F4992E;
+      blk_time = 32'h74749054;
+      blk_nbits = 32'h747B1B18;
+      blk_nonce = 32'h43F740C0;
+      
       fork
 	 begin
             $display("Sending ...");
-            for(int i = 0; i < 20;i++) begin
-               mst_gen_transaction();
-            end  
-            $display("Looped master to slave transfers example with randomization completes");
+
+	    tester #($size(blk_version))::packed_to_unpacked(blk_version, data_out);
+	    tester::print_unpacked(data_out);
+	    gen_transaction(data_out);
+	    
+	    tester #($size(prev_blk_header_hash))::packed_to_unpacked(prev_blk_header_hash, data_out);
+	    tester::print_unpacked(data_out);
+	    gen_transaction(data_out);
+
+	    tester #($size(merkle_root_hash))::packed_to_unpacked(merkle_root_hash, data_out);
+	    tester::print_unpacked(data_out);
+	    gen_transaction(data_out);
+
+	    tester #($size(blk_time))::packed_to_unpacked(blk_time, data_out);
+	    tester::print_unpacked(data_out);
+	    gen_transaction(data_out);
+
+	    tester #($size(blk_nbits))::packed_to_unpacked(blk_nbits, data_out);
+	    tester::print_unpacked(data_out);
+	    gen_transaction(data_out);
+
+	    tester #($size(blk_nonce))::packed_to_unpacked(blk_nonce, data_out);
+	    tester::print_unpacked(data_out);
+	    gen_transaction(data_out);
+
+            $display("Sent all the data...");
 	 end
 	 begin
             slv_gen_tready();
 	 end
       join
       
-
       wait(comparison_cnt == 8);
       
-      if(error_cnt ==0) begin
+      if(error_cnt == 0) begin
 	 $display("EXAMPLE TEST DONE : Test Completed Successfully");
       end else begin  
 	 $display("EXAMPLE TEST DONE ",$sformatf("Test Failed: %d Comparison Failed", error_cnt));
@@ -116,14 +158,6 @@ module tb_main(
       ready_gen.set_high_time(6);
       slv_agent.driver.send_tready(ready_gen);
    endtask :slv_gen_tready
-
-   task mst_gen_transaction();
-      axi4stream_transaction                         wr_transaction; 
-      wr_transaction = mst_agent.driver.create_transaction("Master VIP write transaction");
-      wr_transaction.set_xfer_alignment(XIL_AXI4STREAM_XFER_RANDOM);
-      WR_TRANSACTION_FAIL: assert(wr_transaction.randomize());
-      mst_agent.driver.send(wr_transaction);
-   endtask
 
    initial begin
       forever begin
@@ -170,7 +204,7 @@ module tb_main(
    end // initial begin
 
    /* ******************** */
-   `define  miner  DUT.design_1_i.test_ip_0.inst.miner
+`define  miner  DUT.design_1_i.test_ip_0.inst.miner
 
    always @(posedge `miner.start or negedge `miner.start) begin
       $display("`miner.start changed: %H", `miner.start);
@@ -182,33 +216,61 @@ module tb_main(
       print_miner();
    end
 
-function print_miner();
-   begin
-      $display("Time: %t", $time);
-      $display("miner.blk_version: 0x%H", `miner.blk_version);
-      $display("miner.prev_blk_header_hash: 0x%H", `miner.prev_blk_header_hash);
-      $display("miner.merkle_root_hash: 0x%H", `miner.merkle_root_hash);
-      $display("miner.blk_time: 0x%H", `miner.blk_time);
-      $display("miner.blk_nbits: 0x%H", `miner.blk_nbits);
-      $display("miner.blk_nonce: 0x%H", `miner.blk_nonce);
 
-      $display("miner.bitcoin_blk: 0x%H", `miner.bitcoin_blk);
-      $display("miner.bitcoin_nonce: 0x%H", `miner.bitcoin_nonce);
-      $display("miner.bitcoin_done: 0x%H", `miner.bitcoin_done);
-      $display("");
-   end
-endfunction
+   task automatic gen_rand_transaction(ref axi4stream_transaction wr_transaction);
+      wr_transaction = mst_agent.driver.create_transaction("Master VIP write transaction");
+      wr_transaction.set_xfer_alignment(XIL_AXI4STREAM_XFER_RANDOM);
+      WR_TRANSACTION_FAIL: assert(wr_transaction.randomize());
+   endtask
 
-function print_data(string msg, xil_axi4stream_data_byte data[0:3]);
-   begin
-      $write({msg, " "});
+   // Tasks
+   task gen_transaction(input [7:0] data[]);
+      for (int i = 0; i < $size(data); i = i + 4)
+	begin
+	   xil_axi4stream_data_byte data_dbg[4];
+	   axi4stream_transaction                         wr_transaction; 
 
-      $write("0x");
-      for(int i = $size(data) - 1; i >= 0; i--) begin
-	 $write("%H", data[i]);
+	   gen_rand_transaction(wr_transaction);
+	   wr_transaction.set_data('{data[i+3], data[i+2], data[i+1], data[i]});
+
+	   wr_transaction.get_data(data_dbg);
+	   print_data("Debug: ", data_dbg);
+
+	   mst_agent.driver.send(wr_transaction);
+	end
+   endtask; // gen_transaction
+   
+   
+   // Functions
+   function print_miner();
+      begin
+	 $display("");
+	 $display("Time: %t", $time);
+	 $display("miner.blk_version: 0x%H", `miner.blk_version);
+	 $display("miner.prev_blk_header_hash: 0x%H", `miner.prev_blk_header_hash);
+	 $display("miner.merkle_root_hash: 0x%H", `miner.merkle_root_hash);
+	 $display("miner.blk_time: 0x%H", `miner.blk_time);
+	 $display("miner.blk_nbits: 0x%H", `miner.blk_nbits);
+	 $display("miner.blk_nonce: 0x%H", `miner.blk_nonce);
+
+	 $display("miner.bitcoin_blk: 0x%H", `miner.bitcoin_blk);
+	 $display("miner.bitcoin_nonce: 0x%H", `miner.bitcoin_nonce);
+	 $display("miner.bitcoin_done: 0x%H", `miner.bitcoin_done);
+	 $display("");
       end
-      $display("");
-   end
-endfunction // print_data   
+   endfunction
+
+   function print_data(string msg, xil_axi4stream_data_byte data[4]);
+      begin
+	 $write({msg, " "});
+
+	 // data is stored in litle endian
+	 $write("0x");
+	 for(int i = $size(data) - 1; i >= 0; i--) begin
+	    $write("%H", data[i]);
+	 end
+	 $display("");
+      end
+   endfunction // print_data
 endmodule
 
