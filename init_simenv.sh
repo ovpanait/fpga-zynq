a182c496e24e3b500ae59302c35ca3e0190919de
Ovidiu Panait <ovpanait@gmail.com>
2018-11-16 23:36:39 +0200

    init_simenv.sh: add $SIM_TOP env variable
    - needed by scripts that use tcl scripts in simulation
    
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- init_simenv.sh
+++ init_simenv.sh
@@ -4,6 +4,7 @@ export VIVADO_SDK="${VIVADO_SDK:-$HOME/Xilinx/Tools/Vivado/2018.2/settings64.sh}
 source "${VIVADO_SDK}"
 
 export SCRIPTS_TOP="$(pwd)/Scripts/tcl"
+export SIM_TOP="$SCRIPTS_TOP/Simulation"
 export INCLUDE_DIR="$(pwd)/Examples/include"
 
 export PATH="$PATH:$(ls -1d $SCRIPTS_TOP/*/ | tr "\n" ":")"
