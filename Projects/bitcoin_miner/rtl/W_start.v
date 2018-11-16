c25ed27de474837854c9e750ba15b5340a85239d
Ovidiu Panait <ovpanait@gmail.com>
2018-11-16 23:41:21 +0200

    bitcoin_miner: sha_block: fix implementation
    Fix "Multiple Driver Nets" opt_design errors for nonce_tmp/H_tmp.
    
    Also, remove useless nonce and Hash buffers from W_start module.
    
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- Projects/bitcoin_miner/rtl/W_start.v
+++ Projects/bitcoin_miner/rtl/W_start.v
@@ -7,11 +7,8 @@ module W_start (
 
 		input [`WORD_S-1:0] 	 nonce,
 		input [`WARR_S-1:0] 	 W_in,
-		input [`H_SIZE-1:0] 	 Hin,
 
-		output reg [`WORD_S-1:0] nonce_out,
 		output reg [`WARR_S-1:0] W,
-		output reg [`H_SIZE-1:0] H,
 		output reg 		 en_next
 		);
 
@@ -23,8 +20,6 @@ module W_start (
 	
 	if (reset == 1'b1) begin
 	   en_next <= 1'b0;
-	   H <= {`H_SIZE{1'b0}};
-	   nonce_out <= {`WORD_S{1'b0}};
 	end
 	else begin
 	   if ((en == 1'b1) || (en == 1'b0 && cnt != 5'b0)) begin
@@ -34,8 +29,6 @@ module W_start (
 	   if (cnt == (`DELAY-1))
 	     begin
 		W <= W_in;
-		H <= Hin;
-		nonce_out <= nonce;
 		en_next <= 1'b1;
 	     end
 	end
--- Projects/bitcoin_miner/rtl/sha_block.v
+++ Projects/bitcoin_miner/rtl/sha_block.v
@@ -19,12 +19,11 @@ module sha_block(
    wire 			      en_o[2*64/`DELAY:0];
    wire [`WORD_S-1:0] 		      nonce_tmp[2*64/`DELAY:0];
 
-   wire [`H_SIZE-1:0] 		      H_tmp[2*64/`DELAY:0];
+   wire [`H_SIZE-1:0] 		      H_tmp[64/`DELAY:0];
    wire [`WORD_S-1:0] 		      tmp[64/`DELAY:0];
    wire [`WORD_S-1:0] 		      abc_tmp[64/`DELAY:0][7:0];
 
    genvar 			      i;
-   genvar 			      j;
    
    // W[0] = reverse(M)
    generate
@@ -33,14 +32,7 @@ module sha_block(
       end
    endgenerate
 
-   generate
-      for (i = 0; i < `H_SIZE / `W_SIZE; i=i+1) begin
-	 assign H_tmp[0][i*`W_SIZE +: `W_SIZE] = H_prev[i*`W_SIZE +: `W_SIZE]; 
-      end
-   endgenerate
-
    assign en_o[0] = en;
-   assign nonce_tmp[0] = nonce;
    
    generate
       for (i = 0; i < 16 / `DELAY; i=i+1) begin : M_to_W
@@ -49,14 +41,28 @@ module sha_block(
 		     .reset(reset),
 		     .en(en_o[i]),
 
-		     .nonce(nonce_tmp[i]),
 		     .W_in(W[i]),
-		     .Hin(H_tmp[i]),
 
-		     .nonce_out(nonce_tmp[i+1]),
 		     .W(W[i+1]),
-		     .H(H_tmp[i+1]),
 		     .en_next(en_o[i+1]));
+      end
+   endgenerate
+
+   reg [`H_SIZE-1:0] H_delay;
+   reg [`WORD_S-1:0] nonce_delay;
+   
+   always @(posedge clk) begin
+      if (en == 1'b1) begin
+	 H_delay <= H_prev;
+	 nonce_delay <= nonce;
+      end
+   end
+
+   assign nonce_tmp[0] = nonce_delay;
+
+   generate
+      for (i = 0; i < `H_SIZE / `W_SIZE; i=i+1) begin
+	 assign H_tmp[0][i*`W_SIZE +: `W_SIZE] = H_delay[i*`W_SIZE +: `W_SIZE]; 
       end
    endgenerate
 
@@ -76,7 +82,7 @@ module sha_block(
    // SHA ROUNDS
    generate
       for (i = 0; i < 8; i=i+1) begin
-	 assign  abc_tmp[0][7 - i] = H_tmp[1][i*`W_SIZE +: `W_SIZE]; // shit
+	 assign  abc_tmp[0][7 - i] = H_tmp[0][i*`W_SIZE +: `W_SIZE]; // shit
       end
    endgenerate
 
@@ -87,7 +93,7 @@ module sha_block(
 			 .reset(reset),
 			 .en(en_o[i+1]),
 
-			 .nonce(nonce_tmp[i+1]),
+			 .nonce(nonce_tmp[i]),
 			 .a(abc_tmp[i][0]),
 			 .b(abc_tmp[i][1]),
 			 .c(abc_tmp[i][2]),
@@ -96,9 +102,9 @@ module sha_block(
 			 .f(abc_tmp[i][5]),
 			 .g(abc_tmp[i][6]),
 			 .h(abc_tmp[i][7]),
-			 .Hin(H_tmp[i+1]),
+			 .Hin(H_tmp[i]),
 
-			 .nonce_out(nonce_tmp[i+2]),
+			 .nonce_out(nonce_tmp[i+1]),
 			 .K(K[i*(`DELAY*`W_SIZE) +: `DELAY*`W_SIZE]),
 			 .W(W[i+1] >> i*`DELAY*`WORD_S), // FUCK: left vs right shift..
 
@@ -110,7 +116,7 @@ module sha_block(
 			 .f_next(abc_tmp[i+1][5]),
 			 .g_next(abc_tmp[i+1][6]),
 			 .h_next(abc_tmp[i+1][7]),
-			 .H(H_tmp[i+2]),
+			 .H(H_tmp[i+1]),
 
 			 .en_next(en_o[64/`DELAY + 1 + i])
 			 );
@@ -125,7 +131,7 @@ module sha_block(
 		.reset(reset),
 		.en(en_o[i+1]),
 
-		.nonce(nonce_tmp[i+1]),
+		.nonce(nonce_tmp[i]),
 		.a(abc_tmp[i][0]),
 		.b(abc_tmp[i][1]),
 		.c(abc_tmp[i][2]),
@@ -134,9 +140,9 @@ module sha_block(
 		.f(abc_tmp[i][5]),
 		.g(abc_tmp[i][6]),
 		.h(abc_tmp[i][7]),
-		.Hin(H_tmp[i+1]),
+		.Hin(H_tmp[i]),
 
-		.nonce_out(nonce_tmp[i+2]),
+		.nonce_out(nonce_tmp[i+1]),
 		.K(K[i*(`DELAY*`W_SIZE) +: `DELAY*`W_SIZE]),
 		.W(W[i+1]),
 
@@ -148,7 +154,7 @@ module sha_block(
 		.f_next(abc_tmp[i+1][5]),
 		.g_next(abc_tmp[i+1][6]),
 		.h_next(abc_tmp[i+1][7]),
-		.H(H_tmp[i+2]),
+		.H(H_tmp[i+1]),
 
 		.en_next(en_o[64/`DELAY + 1 + i])
 		);
@@ -161,7 +167,7 @@ module sha_block(
 		      .en(en_o[2*64/`DELAY]),
 
 		      .H_i(H_tmp[64/`DELAY]),
-		      .nonce(nonce_tmp[64/`DELAY+1]),
+		      .nonce(nonce_tmp[64/`DELAY]),
 
 		      .a(abc_tmp[64/`DELAY][0]),
 		      .b(abc_tmp[64/`DELAY][1]),
