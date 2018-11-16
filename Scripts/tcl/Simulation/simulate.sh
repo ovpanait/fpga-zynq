25503419253966ca2875433d4cbfc6930fc5b793
Ovidiu Panait <ovpanait@gmail.com>
2018-11-16 23:38:55 +0200

    Scripts: simulate.sh: add command line parameters parsing and check_impl option
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- Scripts/tcl/Simulation/simulate.sh
+++ Scripts/tcl/Simulation/simulate.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 set -e
 
-
-if [ "$1" == "export_sim" ];then
-    vivado -mode tcl -source "$SCRIPTS_TOP"/Simulation/export_sim.tcl -nolog -nojournal
-fi
+while [ "$1" != "" ];do
+    opt="$1"
+    case $opt in
+	"export_sim")
+	    vivado -mode tcl -source "$SIM_TOP"/export_sim.tcl -nolog -nojournal
+	    ;;
+	"check_impl")
+	    shift
+	    vivado -mode tcl -source "$SIM_TOP"/check_impl.tcl -nolog -nojournal \
+		   -tclargs "$1"
+	    exit 0
+	    ;;
+    esac
+    shift
+done
 
 cd ./outputs/export_sim/xsim
 ./tb_main.sh -reset_run && ./tb_main.sh
+
