797dcfd3c4d303661ec121a64a61d83e568b4135
Ovidiu Panait <ovpanait@gmail.com>
2018-12-01 19:25:31 +0200

    Scripts: create_proj.tcl: DO not fail on missing constraints file
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- Scripts/tcl/Simulation/create_proj.tcl
+++ Scripts/tcl/Simulation/create_proj.tcl
@@ -5,6 +5,6 @@ read_verilog  [ glob ./Sources/*.v ]
 read_verilog [ glob -nocomplain ./Sources/*.sv ] -quiet
 
 #read_verilog [ glob ./Tb/*.sv ]
-add_files -fileset [get_filesets sim_1] -norecurse [ glob ./Tb/*.sv ]
+add_files -fileset [get_filesets sim_1] -norecurse [ glob -nocomplain ./Tb/*.sv ] -quiet
 
 read_xdc [ glob -nocomplain ./Constraints/*.xdc ] -quiet
