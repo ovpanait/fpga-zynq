167b8d0d9c92e0c3db20aa041ca7ad8a270352b0
Ovidiu Panait <ovidiu.panait@windriver.com>
2018-12-10 15:32:25 +0200

    init_simenv.sh: Use find instead of ls
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- init_simenv.sh
+++ init_simenv.sh
@@ -3,10 +3,10 @@
 export VIVADO_SDK="${VIVADO_SDK:-$HOME/Xilinx/Tools/Vivado/2018.2/settings64.sh}"
 source "${VIVADO_SDK}"
 
-export SCRIPTS_TOP="$(pwd)/Scripts/tcl"
+export SCRIPTS_TOP="$(pwd)/Scripts"
 export SIM_TOP="$SCRIPTS_TOP/Simulation"
 export INCLUDE_DIR="$(pwd)/Templates/include"
 
 export TCL_INCLUDE="${SCRIPTS_TOP}/include"
 
-export PATH="$PATH:$(ls -1d $SCRIPTS_TOP/*/ | tr "\n" ":")"
+export PATH="$PATH:$(find $SCRIPTS_TOP -type d | tr "\n" ":")"
