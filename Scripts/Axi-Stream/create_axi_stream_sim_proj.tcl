e5dde36d04d5971b310da83457e188572b93f79a
Ovidiu Panait <ovidiu.panait@windriver.com>
2018-12-10 16:24:15 +0200

    Scripts: create_axi_stream_proj.sh: add --top option
    Signed-off-by: Ovidiu Panait <ovidiu.panait@gmail.com>

--- Scripts/Axi-Stream/create_axi_stream_sim_proj.tcl
+++ Scripts/Axi-Stream/create_axi_stream_sim_proj.tcl
@@ -1,7 +1,8 @@
 set proj_name test_proj
 set proj_path "[pwd]/$proj_name"
 
-set ip_name test_ip
+set top [lindex $argv 0]
+set ip_name $top
 set ip_path "[pwd]/ip_repo/$ip_name"
 
 file mkdir $proj_path
@@ -28,11 +29,11 @@ endgroup
 set_property -dict [list CONFIG.TDATA_NUM_BYTES.VALUE_SRC USER] [get_bd_cells axi4stream_vip_1]
 set_property -dict [list CONFIG.INTERFACE_MODE {SLAVE} CONFIG.TDATA_NUM_BYTES {4}] [get_bd_cells axi4stream_vip_1]
 
-create_bd_cell -type ip -vlnv user.org:user:test_ip:1.0 test_ip_0
+create_bd_cell -type ip -vlnv "user.org:user:$top:1.0" "[set top]_0"
 
 # Create connections
-connect_bd_intf_net [get_bd_intf_pins axi4stream_vip_0/M_AXIS] [get_bd_intf_pins test_ip_0/S00_AXIS]
-connect_bd_intf_net [get_bd_intf_pins test_ip_0/M00_AXIS] [get_bd_intf_pins axi4stream_vip_1/S_AXIS]
+connect_bd_intf_net [get_bd_intf_pins axi4stream_vip_0/M_AXIS] [get_bd_intf_pins [set top]_0/S00_AXIS]
+connect_bd_intf_net [get_bd_intf_pins [set top]_0/M00_AXIS] [get_bd_intf_pins axi4stream_vip_1/S_AXIS]
 
 startgroup
 make_bd_pins_external  [get_bd_cells axi4stream_vip_0]
@@ -43,12 +44,12 @@ set_property name aclk [get_bd_ports aclk_0]
 make_bd_pins_external  [get_bd_pins axi4stream_vip_0/aresetn]
 set_property name aresetn [get_bd_ports aresetn_0]
 
-connect_bd_net [get_bd_pins test_ip_0/m00_axis_aresetn] [get_bd_pins test_ip_0/s00_axis_aresetn]
-connect_bd_net [get_bd_ports aresetn] [get_bd_pins test_ip_0/m00_axis_aresetn]
+connect_bd_net [get_bd_pins [set top]_0/m00_axis_aresetn] [get_bd_pins [set top]_0/s00_axis_aresetn]
+connect_bd_net [get_bd_ports aresetn] [get_bd_pins [set top]_0/m00_axis_aresetn]
 connect_bd_net [get_bd_ports aresetn] [get_bd_pins axi4stream_vip_1/aresetn]
 
-connect_bd_net [get_bd_pins test_ip_0/s00_axis_aclk] [get_bd_pins test_ip_0/m00_axis_aclk]
-connect_bd_net [get_bd_ports aclk] [get_bd_pins test_ip_0/m00_axis_aclk]
+connect_bd_net [get_bd_pins [set top]_0/s00_axis_aclk] [get_bd_pins [set top]_0/m00_axis_aclk]
+connect_bd_net [get_bd_ports aclk] [get_bd_pins [set top]_0/m00_axis_aclk]
 connect_bd_net [get_bd_ports aclk] [get_bd_pins axi4stream_vip_1/aclk]
 
 validate_bd_design -force
