b9da703655969fb3dd4478eb9aabb669c594402e
Ovidiu Panait <ovpanait@gmail.com>
2019-01-13 14:42:39 +0200

    Simulation: synth.tcl: Add post synthesys timing report
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- Scripts/Simulation/synth.tcl
+++ Scripts/Simulation/synth.tcl
@@ -3,3 +3,4 @@ source $env(SIM_TOP)/create_proj.tcl
 set top_name [lindex $argv 0]
 
 synth_design -top $top_name -part xc7z020clg400-1 -flatten rebuilt
+deval report_timing_summary -file $outputDir/post_synth_timing.rpt
