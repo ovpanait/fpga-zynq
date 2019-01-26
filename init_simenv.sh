d45f372ee87ba20b1add1354ee7a72fc90580471
Ovidiu Panait <ovpanait@gmail.com>
2019-01-26 22:29:42 +0200

    Scripts: hdf2dts: Remove hardcoded paths
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- init_simenv.sh
+++ init_simenv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 export VIVADO_SDK="${VIVADO_SDK:-$HOME/Xilinx/Tools/Vivado/2018.2/settings64.sh}"
+export DTX_SDK="${DTX_SDK:-/home/ovidiu/Xilinx/ArtyZ7/device-tree-xlnx}"
+
 source "${VIVADO_SDK}"
 
 export SCRIPTS_TOP="$(pwd)/Scripts"
